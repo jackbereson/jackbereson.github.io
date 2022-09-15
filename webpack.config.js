434fdbbf6cb27ed39b51ea4c863230b3f0dc87bd
Jack Bereson <jackbereson@gmail.com>
2022-09-16 02:40:42 +0700

    Add new files

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/main.js',
+    plugins: [new ESLintPlugin()],
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    }
+};
