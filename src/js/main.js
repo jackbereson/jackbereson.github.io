f6be9785a425220218eb1739c7e114c03d75d95a
Jack (⚓⚓⚓) Sparrow <jackbereson@gmail.com>
2026-08-25 20:52:55 +0700

    Main

--- src/js/main.js
+++
@@ -1,6 +0,0 @@
-import { icryptobookHelper } from "./icryptobook.helper";
-
-(async () => {
-  const res = await icryptobookHelper.getAllCryptoFastNews();
-  console.log("data", res.data.data.getAllCryptoFastNews.data);
-})();
